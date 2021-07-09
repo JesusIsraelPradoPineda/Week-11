7564b13eacc601c366b5121dd25643f92e36e050
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-07-09 08:11:00 -0500

    Chapter 13 Ex 01 finished

---
+++ Chapter13ex01.c
@@ -0,0 +1,25 @@
+/********************************************************
+ * ExOriginal -- Change to zeros. *
+ * Not an especially earth-shattering program. *
+ * *
+ * Author: Israel Prado. *
+ * *
+ * Purpose: To know how to use punters in C. *
+ * *
+ * Usage: *
+ * Put the values in the central programation and then you will show the values  *
+ ********************************************************/
+#include <stdio.h> //I defined the library
+#define arr 5 //I define the number of the values in the array
+int main()
+{
+	int val[arr] = {2, 4, 5, 6, 8}; //genero e ingreso los valores dentro de un array
+	int *xd; //variable para acceder a la memoria donde se ubican los datos del array
+	for (xd = &val[0]; xd <= &val[arr]; ++xd) { //xd is inicialited in the array value 0, it finish when it reaches the number estableced in "arr"
+		*xd = 0; //the values that we have, it will modifie by 0 in all the memory side
+	}
+;
+	for (int i = 1; i <= arr; ++i) //I show all the numbers 0, counting all the numbers that we have {
+		printf("data[%d] = %d\n", i, val[i]); //I show the numbers and I count that
+	return 0;
+}
